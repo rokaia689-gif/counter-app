02e79483e185bacc5b94ed750082609735be029c
rokaia689-gif <rokaia689@gmail.com>
2026-07-18 18:09:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-alert("hello");
-let counter=0 ;
-let count =document.getElementById("count");
-let tasbeeh=document.getElementById("tasbeeh");
+let counter=0;
+let count=document.getElementById("count");
+let increase=document.getElementById("increase");
+let decrease=document.getElementById("decrease");
 let reset=document.getElementById("reset");
-let text=document.getElementById("text");
-
-function changealzeker(){
-    if(counter==33){
-        text.innerHTML="الحمدلله";
-    }
-    else if(counter==66){
-        text.innerHTML="الله أكبر";
-    }
-    else if(counter==99){
-        text.innerHTML="تم قراءة الأذكار بنجاح"
-        counter=0;
-        count.innerHTML=counter;
-    }
+function changecolor(){
+    if (counter>0){
+   count.style.color="green"}
+else if(counter<0){
+count.style.color="red";
+}
+else
+{count.style.color="black"};
 }
 
-tasbeeh.addEventListener("click",function(){
-    counter++;
-    count.innerHTML=counter
-    changealzeker();
 
-});
-reset.addEventListener("click",function(){
+
+increase.onclick=function(){
+    counter++;
+    count.innerHTML=counter;
+    changecolor();
+}
+decrease.onclick=function(){
+    counter--;
+    count.innerHTML=counter;
+    changecolor(); 
+}
+reset.onclick=function(){
     counter=0;
-    count.innerHTML=counter
-    text.innerHTML="سبحان الله";
-});
-    
+    count.innerHTML=counter;
+    changecolor(); 
+}
